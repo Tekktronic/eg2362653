353f12b3878e163ba3bc587dc9b56002f6b5c89d
tekktronic <ezgmusic@gmail.com>
2014-06-02 06:51:31 -0700

    Signed-off-by: tekktronic <ezgmusic@gmail.com>

--- CSC-5_Final_Project_Commando_v1.0_alpha/build/Debug/Cygwin_4.x-Windows/TileObj.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/TileObj.o: TileObj.cpp TileObj.h Player.h
-
-TileObj.h:
-
-Player.h:
--- CSC-5_Final_Project_Commando_v1.0_alpha/build/Debug/Cygwin_4.x-Windows/main.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/main.o: main.cpp Player.h TileObj.h \
- Weapon.h
-
-Player.h:
-
-TileObj.h:
-
-Weapon.h:
