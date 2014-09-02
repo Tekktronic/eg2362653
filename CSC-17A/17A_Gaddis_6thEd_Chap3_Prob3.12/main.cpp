c63a928146f669f431ea35167a08a525f29143fd
Tekktronic <ezgmusic@gmail.com>
2014-09-02 09:57:49 -0700

    Created the following New Folders: CSC-11 CSC-17A CSC-17B
    Signed-off-by: Tekktronic <ezgmusic@gmail.com>

---
+++ CSC-17A/17A_Gaddis_6thEd_Chap3_Prob3.12/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * NAME: Ezra P. Guinmapang
+ * DATE: September 2, 2014, 9:32 AM
+ * PURPOSE: Solve Gaddis, 6thEd, Chap3, Prob 3.12
+ */
+
+//Libraries
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+//Global variables and constants
+
+//Function prototypes
+
+//Execution begins HERE
+int main(int argc, char** argv) {
+    //declare variables
+    unsigned short month = 0;   //user input: month
+    unsigned short year = 0;    //user input: year
+    float mSale = 0;            //user input: Total Monthly sales w/ sales tax
+    float stRate = 0.04f;       //state sales tax rate
+    float ctRate = 0.06f;       //county sales tax rate
+    float stTax = 0;            //state sales tax amount
+    float ctTax = 0;            //county sales tax amount
+    float totTax = 0;           //Total sales tax amount
+    float totSale = 0;          //Total Monthly Sales w/o sales tax
+    //input values
+    
+    //calculate results
+    
+    //display output
+    
+    //Execution ends HERE
+    return 0;
+}
+
